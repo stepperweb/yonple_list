1ba54d5ea6960b0a4579d1161b75a1c51597e7aa
stepperweb <wodnqjel@gmail.com>
2021-07-02 23:02:35 +0900

    refix api

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,9 +1,10 @@
-export const getLists = async (page) => {
+export const getLists = async (page, type) => {
   const rootApi = "https://recruit-api.yonple.com/recruit";
   const accessToken = process.env.REACT_APP_ACCESSTOKEN;
 
   const lists = await (
-    await fetch(`${rootApi}/${accessToken}/a-posts?page=${page}`)
+    await fetch(`${rootApi}/${accessToken}/${type}-posts?page=${page}`)
   ).json();
+  console.log(lists);
   return lists;
 };
