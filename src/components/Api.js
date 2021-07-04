80c875c277312c95f3046baeca06fd31db180347
stepperweb <wodnqjel@gmail.com>
2021-07-04 15:24:23 +0900

    fix api

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,10 +1,9 @@
-export const getLists = async (page, type) => {
+export const getLists = async (page) => {
   const rootApi = "https://recruit-api.yonple.com/recruit";
   const accessToken = process.env.REACT_APP_ACCESSTOKEN;
 
   const lists = await (
-    await fetch(`${rootApi}/${accessToken}/${type}-posts?page=${page}`)
+    await fetch(`${rootApi}/${accessToken}/a-posts?page=${page}`)
   ).json();
-  console.log(lists);
   return lists;
 };
